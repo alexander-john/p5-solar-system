1a9173671f035976fc288da18284950c89fa731f
Alexander John <96aljohn@gmail.com>
2024-08-25 16:22:50 -0700

    edit-translate

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
+let sun;
+
 function setup() {
   createCanvas(700, 700);
+  sun = new Body(100, createVector(0,0), createVector(0,0));
 }
 
 function draw() {
+  translate(width/2, height/2);
   background(100);
+  sun.show();
 }
 
 function Body(_mass, _pos, _vel) {
