930988210e4f7c8cdade43e153be44fc110bf058
Alexander John <96aljohn@gmail.com>
2024-08-25 16:18:28 -0700

    update-sketch

--- sketch.js
+++ sketch.js
@@ -5,3 +5,16 @@ function setup() {
 function draw() {
   background(100);
 }
+
+function Body(_mass, _pos, _vel) {
+  this.mass = _mass;
+  this.pos = _pos;
+  this.vel = _vel;
+  this.r = this.mass;
+
+  this.show = function() {
+    noStroke();
+    fill(255);
+    ellipse(this.pos.x, this.pos.y, this.r)
+  }
+}
